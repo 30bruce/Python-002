3ca0ae924d30011daf28ee71c713f520321a54c4
30bruce <snowball0@qq.com>
2020-08-09 23:57:54 +0800

    homework - update

--- week03/pmap.py
+++ week03/pmap.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import time
 import socket
+import argparse
 from functools import partial
+from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
 
-kv = {}
 
 def ping(ip):
     res = os.system(f"ping -c 1 -W0 {ip} > /dev/null 2>&1")
@@ -28,7 +30,7 @@ def get_ips(arg_ip):
         ip_prefix = '.'.join(ip_splits[:-1])
         min_ip_int, max_ip_int = int(ip_splits[-1]), int(tmp[1].split('.')[-1])
         ips = []
-        max_ip_int = min(255, max_ip_int)
+        max_ip_int = min(255, max_ip_int)   # 最大不超过255
         for i in range(min_ip_int, max_ip_int+1):
             ips.append(f'{ip_prefix}.{i}')
     return ips
@@ -36,38 +38,47 @@ def get_ips(arg_ip):
 
 if __name__ == '__main__':
     # print(sys.argv)
-    arg_it = iter(sys.argv[1:])
-    kv = dict(zip(arg_it, arg_it))
-    ip_val = kv.get('-ip', '')
+    args = sys.argv[1:]
+    # opts, args = getopt.getopt(sys.argv[1:], '-ip:-n:-f:-m:-v')
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-v', required=False, nargs='*')
+    for i in ('-ip', '-n', '-f','-m', '-w'):
+        arg_parser.add_argument(i, required=False)
+    arg = arg_parser.parse_args()
+    start = time.time()
+    ip_val = arg.ip
     if not ip_val:   # 必须有指定ip才能运行
         sys.exit('请输入指定ip')
 
     ips = get_ips(ip_val)
     # print(ips)
-    n = int(kv.get('-n', 1))   # 默认开启一个线程
-    f = kv.get('-f', 'ping')
+    n = int(arg.n)   # 默认开启一个线程
+    f = arg.f
     res = []
+    m = arg.m
+    if m == 'proc':
+        m_pool = Pool
+    else:
+        m_pool = ThreadPoolExecutor
     if f == 'tcp':
         ports = (i+1 for i in range(1024))
-        with ThreadPoolExecutor(n) as executor:
+        with m_pool(n) as executor:
             func = partial(tcp, ips[0])
             res = executor.map(func, ports)
     else:  # 默认执行 ping
         res = []
-        with ThreadPoolExecutor(n) as executor:
+        with m_pool(n) as executor:
             res = executor.map(ping, ips)
     
     if res:
         res = '\n'.join([f'{i}' for i in res if i])
         print(res)
-        w_path = kv.get('-w', '')
+        w_path = arg.w
         if w_path:
             with open(w_path, 'w') as f:
                 f.write(res)
-        
-    
-    
+    if arg.v is not None:
+        print(f'Excution tiem {time.time()-start}')
 
-    
 
 
